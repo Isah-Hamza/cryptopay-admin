6451fd8862ecf9a197754b47a149adde3b474dca
Isah Hamza <itshamzy@gmail.com>
2024-08-08 14:28:31 +0100

    -

--- src/pages/Main/Referrers/index.jsx
+++ src/pages/Main/Referrers/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import Input from '../../../components/Inputs'
-import { BiCopy, BiCopyAlt, BiPhoneIncoming, BiSearch, BiUser } from 'react-icons/bi'
+import { BiCopy, BiCopyAlt, BiPhoneIncoming, BiSearch, BiTrash, BiUser } from 'react-icons/bi'
 import Select from '../../../components/Inputs/Select'
 import Button from '../../../components/Button'
 import { CgClose } from 'react-icons/cg'
@@ -10,17 +10,21 @@ import completed from '../../../assets/images/completed.svg'
 import New from '../../../components/Referral/New'
 import { useLocation } from 'react-router-dom'
 import reactivateIcon from '../../../assets/images/reactivate.svg'
+import deactivateIcon from '../../../assets/images/deactivate_user.svg'
 
 const Referrers = () => {
     
     const query = useLocation().search.split('=')[1];
     const [acitveTab, setActiveTab] = useState(0);
+    const [acitveInnerTab, setActiveInnerTab] = useState(0);
 
     const [viewDetails, setViewDetails] = useState(false);
     const [reactivate, setReactivate] = useState(false);
+    const [deactivate, setDeactivate] = useState(false);
 
     const toggleViewDetails = () => setViewDetails(!viewDetails);
     const toggleReactivate = () => setReactivate(!reactivate);
+    const toggleDeactivate = () => setDeactivate(!deactivate);
 
     const dummy_deactivated = [
         {
@@ -59,6 +63,7 @@ const Referrers = () => {
             reason:"Build roll that's crack but functional boardroom expectations so third. Break place dogpile scope line reality bed future-proof."
         },
     ]
+
     const dummy = [
         {
             name:'Marcia Cronin ',
@@ -123,29 +128,57 @@ const Referrers = () => {
             date:'09/10/2024',
             refer:'Stanley Stacey',
             test:3,
-            status:'completd',
-            amount:'₦2,800,000',
+            status:'paid',
+            amount:'₦80,000',
         },
         {
             date:'12/01/2023',
             refer:'Stanley Stacey',
             test:3,
-            status:'completd',
-            amount:'₦2,800,000',
+            status:'paid',
+            amount:'₦28,000',
         },
         {
             date:'09/10/2024',
             refer:'Hilda Bacci',
             test:3,
-            status:'completd',
+            status:'pending',
             amount:'₦44,000',
         },
         {
             date:'12/01/2023',
             refer:'Emunne Ijeoma',
             test:3,
-            status:'completd',
+            status:'pending',
             amount:'₦26,500',
+        },
+    ]
+
+    const dummyDetails2 = [
+        {
+            refer:'Stanley Stacey',
+            recurring:3,
+            completed_tests:390,
+        },
+        {
+            refer:'Beverly Weimann',
+            recurring:1,
+            completed_tests:21,
+        },
+        {
+            refer:'Ramona Witting',
+            recurring:5,
+            completed_tests:11,
+        },
+        {
+            refer:'Jerome Prohaska',
+            recurring:9,
+            completed_tests:35,
+        },
+        {
+            refer:'Brendan Schoen',
+            recurring:27,
+            completed_tests:44,
         },
     ]
 
@@ -160,13 +193,7 @@ const Referrers = () => {
         },
     ]
 
-    const details = [
-        {
-
-        }
-    ]
-
-
+    const details = []
 
   return (
   <>
@@ -265,7 +292,17 @@ const Referrers = () => {
                         </div>
                     </div>
                 </div>
-                <div className="p-5 text-sm">
+                <div className="relative pt-5 border-b pb-5">
+                    <div className={`transition-all duration-300 absolute h-0.5 w-28 bg-primary left-2.5 bottom-0 ${acitveInnerTab == 1 && '!left-[131px] !w-20'} ${acitveInnerTab == 2 && '!left-[220px] w-[95px]'}`}></div>
+                    <div className="flex gap-7 text-sm pl-4">
+                        {
+                            ['Rebate History', 'Referrals', 'User Details'].map((item, idx) => (
+                                <button onClick={() => setActiveInnerTab(idx)} className={`opacity-70  ${acitveInnerTab==idx && 'font-semibold opacity-100'}`} key={idx}>{item}</button>
+                            ))
+                        }
+                    </div>
+                </div>
+                {acitveInnerTab !== 2 ? <div className="p-5 text-sm">
                     <div className="mt-3 grid grid-cols-2 gap-5">
                         {
                             test_stats.map((item,idx) => (
@@ -276,14 +313,14 @@ const Referrers = () => {
                             ))
                         }
                     </div>
-                </div>
-                <div className="mt-5 text-[13px]">
+                </div> : null }
+                <div className={`mt-5 text-[13px] hidden ${acitveInnerTab == 0 && '!block'}`}>
                     <div className="header grid grid-cols-6 gap-3 px-5 font-medium">
                         <p className='line-clamp-1' >Date</p>
-                        <p className='line-clamp-1' >Referrer</p>
+                        <p className='line-clamp-1' >Referral</p>
                         <p className='' >Test</p>
+                        <p className='' >Rebate</p>
                         <p className='' >Status</p>
-                        <p className='' >Amount</p>
                         <p className='' >Action</p>
                     </div>
                     <div className="data  text-text_color mt-3 mb-10">
@@ -293,14 +330,75 @@ const Referrers = () => {
                             <p className='line-clamp-1' >{item.date}</p>
                             <p className='line-clamp-1' >{item.refer}</p>
                             <p className='' >{item.test}</p>
-                            <p className='' >{item.status}</p>
                             <p className='' >{item.amount}</p>
+                            <p className='' >{item.status}</p>
                             <p onClick={toggleViewDetails} className='font-semibold text-light_blue cursor-pointer pl-2' >View</p>
                             </div>
                             )) 
                         }
 
                     </div>
+                </div>
+                <div className={`mt-5 text-[13px] hidden ${acitveInnerTab == 1 && '!block'}`}>
+                    <div className="header grid grid-cols-5 gap-3 px-5 font-medium">
+                        <p className='line-clamp-1 col-span-2' >Referral</p>
+                        <p className='' >Recurring</p>
+                        <p className='' >Completed Tests</p>
+                        <p className='' >Action</p>
+                    </div>
+                    <div className="data  text-text_color mt-3 mb-10">
+                        {
+                            dummyDetails2.map((item,idx) => (
+                            <div key={idx} className={`${idx % 2 !== 1 && 'bg-[#f9f9f9]'} header grid grid-cols-5 gap-3 px-5 py-6 font-medium`}>
+                            <p className='line-clamp-1 col-span-2' >{item.refer}</p>
+                            <p className='line-clamp-1' >{item.recurring}</p>
+                            <p className='' >{item.completed_tests}</p>
+                            <p onClick={null} className='font-semibold text-light_blue cursor-pointer pl-2' >View</p>
+                            </div>
+                            )) 
+                        }
+
+                    </div>
+                </div>
+                <div className={`mt-5 text-[13px] hidden ${acitveInnerTab == 2 && '!block'} pb-5`}>
+                     <div className="px-5 text-base">
+                        <p className='text-base font-semibold'>Other Information</p>
+                        <div className="flex gap-2 mt-3 text-sm">
+                            <p className='font-medium' >Registration Date:</p>
+                            <p className='line-clamp-1' >July 12, 2024</p>
+                        </div>
+                        <div className="flex gap-2 mt-3 text-sm">
+                            <p className='font-medium' >Hospital Name:</p>
+                            <p className='line-clamp-1' >John Doe Hospital</p>
+                        </div>
+                        <div className="flex gap-2 mt-3 text-sm">
+                            <p className='font-medium' >Location:</p>
+                            <p className='line-clamp-1' >N/A</p>
+                        </div>
+                        <div className="flex gap-2 mt-3 text-sm">
+                            <p className='font-medium' >Professinal Title:</p>
+                            <p className='line-clamp-1' >Gynecologist</p>
+                        </div>
+                    </div>
+                    <div className="mt-5 px-5 text-base">
+                        <p className='text-base font-semibold'>Payout Information</p>
+                        <div className="flex gap-2 mt-3 text-sm">
+                            <p className='font-medium' >Account Name:</p>
+                            <p className='line-clamp-1' >John Doe</p>
+                        </div>
+                        <div className="flex gap-2 mt-3 text-sm">
+                            <p className='font-medium' >Account Number:</p>
+                            <p className='line-clamp-1' > 1234 - 5678 - 901</p>
+                        </div>
+                        <div className="flex gap-2 mt-3 text-sm">
+                            <p className='font-medium' >Bank Number:</p>
+                            <p className='line-clamp-1' >Lifebridge Bank PLC</p>
+                        </div>
+                        <button onClick={toggleDeactivate} className="flex text-red-700 font-semibold items-center gap-2 my-6 text-sm">
+                            <BiTrash size={18} className='' /> <span>Deactivate Account</span>
+                        </button>
+                    </div>
+                   
                 </div>
             </div>
         </div> : null}
@@ -314,6 +412,22 @@ const Referrers = () => {
                    <div className="mt-10 flex items-center gap-5 ">
                    <Button onClick={toggleReactivate} className={'!px-5 !bg-white !text-text_color border border-text_color '} title={'Cancel'} />
                    <Button onClick={toggleReactivate} className={'!px-5 !bg-light_blue'} title={'Yes Proceed'} />
+                   </div>
+                 </div>
+               </div> : null
+        }
+        {
+            deactivate ? 
+               <div className='bg-black/50 fixed inset-0 grid place-content-center' >
+                 <div className="bg-white w-[350px] p-5 rounded-2xl flex flex-col justify-center text-center gap-3 text-sm">
+                   <img className='w-12 m-auto' src={deactivateIcon} alt="delete" />
+                   <p className='text-base font-semibold' >Deactivate User</p>
+                   <div className='text-left mt-7'>
+                     <Input label={'Deactivation reason'} placeholder={'Enter reason..'} />
+                   </div>
+                   <div className="mt-10 flex items-center gap-5 ">
+                   <Button onClick={toggleDeactivate} className={'!px-5 !bg-white !text-text_color border border-text_color '} title={'Cancel'} />
+                   <Button onClick={toggleDeactivate} className={'!px-5 bg-red-600'} title={'Yes Proceed'} />
                    </div>
                  </div>
                </div> : null
