631b69a2642e0029fd2781bf92bbc95724ff15c9
Isah Hamza <itshamzy@gmail.com>
2024-08-25 20:44:16 +0100

    -

---
+++ src/api/axiosClient.js
@@ -0,0 +1,52 @@
+import axios from "axios";
+
+const DEBUG = process.env.NODE_ENV === "development";
+
+export const axiosClient = () => {
+  let axiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_BASE_URL,
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  
+  axiosInstance.interceptors.request.use(
+    (config) => {
+      let token = window.localStorage.getItem("referrer-token") ?? JSON.parse(window.localStorage.getItem("referrer-user"))?.token ;
+      // let token = JSON.parse(window.localStorage.getItem("referrer-user"))?.token;
+
+      if (token !== null && typeof token !== 'string' && token !== 'undefined' && token !== undefined) {
+        token = JSON.parse(token);
+      }
+
+      config.headers.Authorization = `Bearer ${token}`;
+      return config;
+    },
+    (error) => {
+      if (DEBUG) {
+        console.error("✉️ ", error);
+      }
+      if(error?.response?.status == 401){
+        window.location.href = '/login';
+      }
+      return Promise.reject(error);
+    }
+  );
+
+  axiosInstance.interceptors.response.use(
+    (response) => {
+      if (response.status === 401) {
+        alert("You are not authorized");
+      }
+      return response;
+    },
+    (error) => {
+      if (error.response && error.response.data) {
+        return Promise.reject(error.response.data);
+      }
+      return Promise.reject(error.message);
+    }
+  );
+
+  return axiosInstance;
+};
---
+++ src/api/endpoints.js
@@ -0,0 +1,37 @@
+const endpoints = {
+  auth: {
+    LOGIN:'login',
+    REGISTER:'register',
+    SETUP_PROFILE:'set-up-profile',
+    FORGOT_PASSWORD:'forgot-password',
+    VERIFY_OTP:'verify-otp',
+    CHANGE_PASSWORD:'change-password',
+  },
+  bank:{
+    ALL_BANKS:'bank/all',
+    VERIFY_ACCOUNT:'bank/verify-account',
+  },
+  dashbaord:{
+    ACTIVITIES:'activities',
+    REBATE_EARNINGS:'rebate-earning',
+    DASHBOARD_DETAILS:'dashboard-details',
+  },
+  referrals:{
+    GET_REFERRALS:'referrals',
+    GET_REFERRAL:'view-referrals',
+    CREATE_REFERRAL:'add-referral',
+    TEST_CATEGORIES:'category/all',
+    CATEGORY_TESTS:'medical-test/view'
+  },
+  payment:{
+    ALL_TRANSACTIONS:'all-transactions'
+  },
+  profile:{
+    GET_PROFILE:'doctor/profile-details',
+    UPDATE_PASSWORD:'doctor/password-update',
+    UPDATE_ACCOUNT:'doctor/account-update',
+    UPDATE_PROFILE:'doctor/profile-update',
+  }
+};
+
+export default endpoints;
